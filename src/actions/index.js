427d6f1160e177b6d91ded881db68048019925d8
primocodetoday <primocodetoday@gmail.com>
2020-05-14 14:05:36 +0200

    Added ADD_ITEM

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,3 +4,16 @@
     payload: { id },
   };
 };
+
+export const addItem = (itemContent) => {
+  const getId = () => Math.floor(Math.random() * (9999 - 1 + 1)) + 1;
+  return {
+    type: 'ADD_ITEM',
+    payload: {
+      item: {
+        id: getId(),
+        ...itemContent,
+      },
+    },
+  };
+};
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,6 +4,8 @@ const initialState = [...initialStore];
 
 const rootReducer = (state = initialState, action) => {
   switch (action.type) {
+    case 'ADD_ITEM':
+      return [...state, action.payload.item];
     case 'REMOVE_ITEM':
       return [...state.filter((item) => item.id !== action.payload.id)];
     default:
