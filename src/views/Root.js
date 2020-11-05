0a112e354e608e37bc3aa055c8dc7d04f6a8698f
primocodetoday <primocodetoday@gmail.com>
2020-11-05 14:39:43 +0100

    nit: Added React.lazy to Root

--- src/views/Root.js
+++ src/views/Root.js
@@ -2,20 +2,18 @@
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import { Provider, useSelector } from 'react-redux';
 import { ReactReduxFirebaseProvider, isLoaded } from 'react-redux-firebase';
-import {
-  PantryPage,
-  ShopListPage,
-  SettingsPage,
-  ItemPage,
-  NewPage,
-  SignIn,
-  SignUp,
-} from 'views';
+import { SettingsPage, SignIn, SignUp } from 'views';
 import { routes } from 'routes';
 import { store, rrfProps } from 'store';
 import { MainTemplate } from 'templates';
 import LoadingScreen from 'helpers/LoadingScreen';
 import PrivateRoute from 'helpers/PrivateRoute';
+
+// Lazy loading views
+const PantryPage = React.lazy(() => import('views/PantryPage/PantryPage'));
+const ShopListPage = React.lazy(() => import('views/ShopListPage/ShopListPage'));
+const ItemPage = React.lazy(() => import('views/ItemPage/ItemPage'));
+const NewPage = React.lazy(() => import('views/NewPage/NewPage'));
 
 const AuthIsLoaded = ({ children }) => {
   const auth = useSelector((state) => state.firebase.auth);
@@ -32,22 +30,16 @@ const Root = () => {
           <BrowserRouter>
             <MainTemplate>
               <Switch>
-                <Route
-                  exact
-                  path={routes.home}
-                  render={() => <Redirect to="/pantry" />}
-                />
-                <PrivateRoute
-                  exact
-                  path={routes.pantry}
-                  component={PantryPage}
-                />
-                <PrivateRoute path={routes.pantryitem} component={ItemPage} />
-                <PrivateRoute path={routes.shoplist} component={ShopListPage} />
-                <PrivateRoute path={routes.new} component={NewPage} />
-                <PrivateRoute path={routes.settings} component={SettingsPage} />
-                <Route path={routes.signIn} component={SignIn} />
-                <Route path={routes.signUp} component={SignUp} />
+                <React.Suspense fallback={<LoadingScreen />}>
+                  <Route exact path={routes.home} render={() => <Redirect to="/pantry" />} />
+                  <PrivateRoute exact path={routes.pantry} component={PantryPage} />
+                  <PrivateRoute path={routes.pantryitem} component={ItemPage} />
+                  <PrivateRoute path={routes.shoplist} component={ShopListPage} />
+                  <PrivateRoute path={routes.new} component={NewPage} />
+                  <PrivateRoute path={routes.settings} component={SettingsPage} />
+                  <Route path={routes.signIn} component={SignIn} />
+                  <Route path={routes.signUp} component={SignUp} />
+                </React.Suspense>
               </Switch>
             </MainTemplate>
           </BrowserRouter>
