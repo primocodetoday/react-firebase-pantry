154919238175221e677b74a4ab5e35ffa0644c9c
primocodetoday <primocodetoday@gmail.com>
2020-11-24 09:22:54 +0100

    ref: mapStateToProps logic moved to hooks

--- src/views/ItemPage/ItemPage.js
+++ src/views/ItemPage/ItemPage.js
@@ -1,14 +1,25 @@
 ﻿import * as React from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { firestoreConnect } from 'react-redux-firebase';
-import { compose } from 'redux';
+import { useSelector } from 'react-redux';
+import { useFirestoreConnect } from 'react-redux-firebase';
 import { ItemTemplate, UserTemplate, InfoTemplate } from 'templates';
 import { Paragraph, Section } from 'atoms';
 import { useParams } from 'react-router-dom';
 
-const ItemPage = ({ item, units }) => {
+const ItemPage = () => {
   const { id } = useParams();
+
+  // sync collections
+  useFirestoreConnect([{ collection: 'pantry' }, { collection: 'settings' }]);
+
+  const item = useSelector((state) => {
+    const { pantry } = state.firestore.data;
+    return pantry ? pantry[id] : null;
+  });
+
+  const units = useSelector((state) => {
+    const { settings } = state.firestore.ordered;
+    return settings ? settings[0].units : null;
+  });
 
   return (
     <UserTemplate>
@@ -24,45 +35,18 @@ const ItemPage = ({ item, units }) => {
   );
 };
 
-ItemPage.defaultProps = {
-  item: '',
-  units: '',
-};
+// const mapStateToProps = (state, ownProps) => {
+//   const { id } = ownProps.match.params;
+//   const { pantry } = state.firestore.data;
+//   const item = pantry ? pantry[id] : null;
+//   // extracting Select options
+//   const { settings } = state.firestore.ordered;
+//   const units = settings ? settings[0].units : null;
 
-ItemPage.propTypes = {
-  item: PropTypes.shape({
-    category: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    stock: PropTypes.number.isRequired,
-    unit: PropTypes.string.isRequired,
-    minStock: PropTypes.number.isRequired,
-    maxStock: PropTypes.number.isRequired,
-  }),
-  units: PropTypes.arrayOf(PropTypes.string),
-};
+//   return {
+//     item,
+//     units,
+//   };
+// };
 
-const mapStateToProps = (state, ownProps) => {
-  const { id } = ownProps.match.params;
-  const { pantry } = state.firestore.data;
-  const item = pantry ? pantry[id] : null;
-  // extracting Select options
-  const { settings } = state.firestore.ordered;
-  const units = settings ? settings[0].units : null;
-
-  return {
-    item,
-    units,
-  };
-};
-
-export default compose(
-  connect(mapStateToProps),
-  firestoreConnect([
-    {
-      collection: 'pantry',
-    },
-    {
-      collection: 'settings',
-    },
-  ]),
-)(ItemPage);
+export default ItemPage;
