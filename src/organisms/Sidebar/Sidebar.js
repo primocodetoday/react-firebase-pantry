c54d96e970ca58adc128655b5cc7229ad0185b37
primocodetoday <primocodetoday@gmail.com>
2020-11-25 18:54:48 +0100

    dev: pantry cards animation with delay added

--- src/organisms/Sidebar/Sidebar.js
+++ src/organisms/Sidebar/Sidebar.js
@@ -6,13 +6,10 @@ import { useDispatch } from 'react-redux';
 import { menuItems } from '../../constants/menuItems';
 import { Wrapper } from './Sidebar.styles';
 
-// TODO Maybe make MenuIcon a Link
-
 const Sidebar = () => {
   const dispatch = useDispatch();
 
   const menu = menuItems.map(({ name, route, src, alt }) => {
-    console.log(name, route, src, alt);
     return <MenuIcon key={name} to={route} src={src} alt={alt} />;
   });
 
@@ -20,13 +17,7 @@ const Sidebar = () => {
     <Wrapper>
       <Logo to={ROUTES.HOME} />
       {menu}
-      <MenuIcon
-        key="logout"
-        to="/pantry"
-        onClick={() => dispatch(signOut())}
-        src="/assets/icons/logout.svg"
-        alt="sign out"
-      />
+      <MenuIcon key="logout" to="" onClick={() => dispatch(signOut())} src="/assets/icons/logout.svg" alt="sign out" />
     </Wrapper>
   );
 };
--- src/views/PantryPage/PantryPage.styles.js
+++ src/views/PantryPage/PantryPage.styles.js
@@ -1,9 +1,41 @@
-﻿import styled from 'styled-components';
+﻿import styled, { css } from 'styled-components/macro';
+
+function delayAppear() {
+  let styles = '';
+  for (let i = 1; i < 7; i += 1) {
+    styles += `
+       > div:nth-child(${i}) {
+         animation-delay: ${i * 0.2}s;
+       }
+     `;
+  }
+  return css`
+    ${styles}
+  `;
+}
 
 export const GridWrapper = styled.div`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   gap: 25px;
+
+  > div {
+    animation: appear 0.2s;
+    animation-fill-mode: both;
+  }
+
+  ${delayAppear()}
+
+  @keyframes appear {
+    0% {
+      opacity: 0;
+      top: +50px;
+    }
+    100% {
+      opacity: 1;
+      top: 0px;
+    }
+  }
 
   @media (max-width: 1350px) {
     grid-template-columns: repeat(2, 1fr);
